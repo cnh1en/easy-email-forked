cb4b5f0289fb81eb76d4012110c2ae7ecb2ca546
cnh1en <cnhien12@gmail.com>
2024-01-02 09:25:12 +0700

    fix

--- packages/easy-email-extensions/src/components/Providers/PolarisProvider.tsx
+++ packages/easy-email-extensions/src/components/Providers/PolarisProvider.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { AppProvider } from '@shopify/polaris';
-import '@shopify/polaris/build/esm/styles.css';
 import translations from '@shopify/polaris/locales/en.json';
 
 const PolarisProvider = ({ children }: { children: React.ReactNode }) => {
